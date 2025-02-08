4c076a24136ff0b2ad0c13356bc049aee212c78c
jdubz789 <le5_jw@hotmail.co.uk>
2025-02-08 07:37:37 -0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+    var backToTopButton = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+document.getElementById("back-to-top").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
